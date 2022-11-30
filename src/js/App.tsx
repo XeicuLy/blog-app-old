6b3f1e9d199186a5774d390d61476896c8dd9814
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2022-11-30 12:00:29 +0900

    :sparkles: feat: ルーティング機能実装

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { CreatePost } from './components/CreatePost';
+import { Home } from './components/Home';
+import { Login } from './components/Login';
+import { Logout } from './components/Logout';
+
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/createpost' element={<CreatePost />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/logout' element={<Logout />}></Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
