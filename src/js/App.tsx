654c8ce9f4678cc25cfae3be536c0b98b7650b37
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2022-11-30 14:13:55 +0900

    :memo: add: ナビゲーションバー実装

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -3,10 +3,12 @@ import { CreatePost } from './components/CreatePost';
 import { Home } from './components/Home';
 import { Login } from './components/Login';
 import { Logout } from './components/Logout';
+import { Navbar } from './components/Navbar';
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/createpost' element={<CreatePost />}></Route>
